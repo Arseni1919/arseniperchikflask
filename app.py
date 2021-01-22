80a47651b4fad2667ed8a8682a84b36f6d01c114
Arseni <1919ars@gmail.com>
2021-01-22 12:07:18 +0200

    comment

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # app.py
 from flask import Flask, request, jsonify, render_template, url_for, redirect
 import pandas as pd
+from threading import Thread
+import time
+
 
 app = Flask(__name__)
+
 
 @app.route('/getmsg/', methods=['GET'])
 def respond():
@@ -27,21 +31,32 @@ def respond():
     # Return the response in json format
     return jsonify(response)
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {param} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+
+@app.route('/config')
+def config_func():
+    for k in app.config:
+        print(f'{k}')
+    print(type(app.config))
+    return 'done'
+
+
+@app.route('/thread')
+def threaded_task(duration):
+    for i in range(duration):
+        print("Working... {}/{}".format(i + 1, duration))
+        time.sleep(1)
+    return 'aaa'
+
+
+@app.route("/task/", defaults={'duration': 5})
+@app.route("/task/<int:duration>")
+def task_func(duration):
+    thread = Thread(target=threaded_task, args=(duration,))
+    thread.daemon = True
+    thread.start()
+    return jsonify({'thread_name': str(thread.name),
+                    'started': True})
+
 
 @app.route('/data')
 def data():
@@ -53,12 +68,15 @@ def data():
         print(data_dict[stock].shape)
     return render_template('data.html', data=data_dict)
 
+
 # A welcome message to test our server
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     # app.run(threaded=True, port=5000)
+    app.run(debug=True)
     app.run()
